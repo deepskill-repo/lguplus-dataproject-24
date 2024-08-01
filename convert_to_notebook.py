c5bddc82183af51d86e2efcac3f77e095fce9215
owen-deepskill <owen.cho@deepskill.io>
2024-08-01 13:57:27 +1000

    team 1 to 3

---
+++ convert_to_notebook.py
@@ -0,0 +1,42 @@
+import nbformat as nbf
+
+def text_to_notebook_cells(text):
+    lines = text.split('\n')
+    cells = []
+    cell_type = 'markdown'
+    cell_content = []
+    
+    for line in lines:
+        if line.startswith('```'):
+            if cell_type == 'markdown':
+                if cell_content:
+                    cells.append(nbf.v4.new_markdown_cell('\n'.join(cell_content)))
+                cell_type = 'code'
+            else:
+                if cell_content:
+                    cells.append(nbf.v4.new_code_cell('\n'.join(cell_content)))
+                cell_type = 'markdown'
+            cell_content = []
+        else:
+            cell_content.append(line)
+    
+    if cell_content:
+        if cell_type == 'markdown':
+            cells.append(nbf.v4.new_markdown_cell('\n'.join(cell_content)))
+        else:
+            cells.append(nbf.v4.new_code_cell('\n'.join(cell_content)))
+    
+    return cells
+
+def read_file_as_string(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content
+
+input_text = read_file_as_string('original')
+cells = text_to_notebook_cells(input_text)
+nb = nbf.v4.new_notebook()
+nb['cells'] = cells
+output_file = '[2조]플래그십인사이트_마케팅믹스모델.ipynb'
+with open(output_file, 'w', encoding='utf-8') as f:
+    nbf.write(nb, f)
